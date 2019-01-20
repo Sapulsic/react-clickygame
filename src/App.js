0785760c483fbab411a3999800f9423a6d50962b
Sapulsic <carlos.barahona1995@gmail.com>
2019-01-20 00:08:11 -0600

    some logical stuff down

--- src/App.js
+++ src/App.js
@@ -8,20 +8,44 @@ import mobpsycho from "./mobpsycho.json";
 
 class App extends Component {
     state = {
-        mobpsycho,
         cScore: 0,
         highScore: 0,
         selectedImg: [],
+        mobpsycho,
         title: "Mob-PsyClick"
-    };
-
-    selectClick = event => {
-        const currentImg = event.target.alt;
-        const currentImgEvent = this.state.selectedImg.indexOf(currentImgEvent) > -1;
     }
 
+    selectClick = event => {
+        this.setState({ cScore: this.state.cScore + 1 });
+        if (this.state.cScore + 1 > this.state.highScore) {
+            this.setState({ 
+                highScore: this.state.highScore + 1,
+                mobpsycho: this.state.mobpsycho.sort((a, b) => {
+                    return 0.5 - Math.random();
+                }),
+                selectedImg: [...this.state.selectedImg],
+            });
+        }
+        
+    }
     
-
+    selectMemory = event => {
+        event.preventDefault();
+        console.log(event.target.id);
+        if (this.state.selectedImg.indexOf(event.target.alt) === -1) {
+            this.state.selectedImg.push(event.target.alt);
+            this.selectClick();
+        }
+        else {
+            this.setState({
+                mobpsycho: this.state.mobpsycho.sort((a, b) => {
+                    return 0.5 - Math.random();
+                }),
+                selectedImg: [],
+                cScore: 0
+            });
+        }
+    }
 
     render() {
         return (
@@ -38,10 +62,11 @@ class App extends Component {
                             id={mobpsycho.id}
                             name={mobpsycho.name}
                             image={mobpsycho.image}
+                            selectMemory={this.selectMemory}
                         />
-                    ))}
+                        ))
+                    }
                 </div>
-                {/* <Footer /> */}
             </Wrapper>
         )
     }
--- src/components/MobCard/MobCard.js
+++ src/components/MobCard/MobCard.js
@@ -2,20 +2,18 @@ import React from "react";
 import "./style.css";
 
 function MobCard(props) {
-    return (
-        <div className="card">
-            <div className="img-container">
-                <img alt={props.name} src={props.image} />
-            </div>
-            <div className="content">
-                {/* <ul>
-                    <li>
-                        <strong>Name:</strong> {props.name}
-                    </li>
-                </ul> */}
-            </div>
+    return <div className="card" key={props.id} onClick={props.selectMemory}>
+        <div className="img-container">
+            <img alt={props.name} src={props.image} />
         </div>
-    );
+        <div className="content">
+            <ul>
+                {/* <li id={props.id}>
+                    <strong>Name:</strong> {props.name}
+                </li> */}
+            </ul>
+        </div>
+    </div>;
 }
 
 export default MobCard;
